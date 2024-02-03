3ecc709fc803f266a8ad32fe8560b5c6467dd3b8
high_flyer_v4 <satis5263@gmail.com>
2024-02-03 20:31:43 +0530

    updated cors

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const { connect } = require('mongoose');
 require('dotenv').config();
 const upload = require('express-fileupload')
@@ -12,9 +11,16 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 app.use(express.json({extended: true}))
 app.use(express.urlencoded({extended: true}))
-app.use(cors({credentials: true, origin: "http://localhost:3000"}))
 app.use(upload())
 app.use('/uploads', express.static(__dirname + '/uploads'))
+
+app.use(express.urlencoded({ extended:true}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
